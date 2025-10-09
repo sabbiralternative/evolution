2e9117c7ee326edfcd8d3431ff33c4f2df0e5918
sabbirHossain <sabbirshnt@gmail.com>
2025-10-09 14:06:46 +0600

    done

--- src/layout/DesktopLayout.jsx
+++ src/layout/DesktopLayout.jsx
@@ -3,11 +3,11 @@ import { Outlet } from "react-router-dom";
 
 const DesktopLayout = () => {
   useEffect(() => {
-    // const root = document.documentElement;
+    const root = document.documentElement;
     // root.style.setProperty("--rem-migration-size", "7.9px");
     // root.style.setProperty("--root-size", "7.526041666666666px");
     // root.style.setProperty("--size", "9.526041666666666px");
-    // root.style.fontSize = "1.43125%";
+    root.style.fontSize = "16px";
     // const body = document.body;
     // body.classList.add("desktop", "SmartLobby--f42fc");
   }, []);
